9a83a88d0fdebcf65f0c7d73c16ee185dccc4f78
Aske Lange <44546482+AskeLange@users.noreply.github.com>
2023-03-26 19:31:26 +0200

    chore: corrections to `package.json` files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,4 @@
 module.exports = {
 	root: true,
-	// extends: '@kinematic-lab',
-
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:vue/vue3-recommended',
-		'plugin:nuxt/recommended',
-		'prettier',
-	],
-	plugins: ['vue', '@typescript-eslint'],
-	parser: 'vue-eslint-parser',
-	parserOptions: {
-		parser: '@typescript-eslint/parser',
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	env: {
-		browser: true,
-		amd: true,
-		node: true,
-	},
-	rules: {
-		indent: ['error', 'tab'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
-		'prefer-destructuring': ['warn', { object: true, array: true }],
-		'no-unused-vars': ['warn'],
-		'no-undef': 0,
-		'vue/no-v-html': 0,
-		'vue/no-v-text-v-html-on-component': 0,
-		'vue/multi-word-component-names': 0,
-	},
+	extends: '@kinematic-lab',
 };
