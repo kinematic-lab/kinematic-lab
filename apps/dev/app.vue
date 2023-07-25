f58b8cc9812dfa98d1bf93caca1bbd52793a3ea8
Aske Lange <44546482+AskeLange@users.noreply.github.com>
2023-07-25 21:11:12 +0200

    chore(`apps/dev`): added test data

--- apps/dev/app.vue
+++ apps/dev/app.vue
@@ -9,6 +9,21 @@ body {
 	font-family: 'PP Air';
 	overscroll-behavior: none;
 
+	& pre {
+		font-family: 'PP Air Mono';
+		line-height: 1.25;
+		margin: 0;
+	}
+
+	& h1,
+	& h2,
+	& h3,
+	& h4,
+	& h5,
+	& h6 {
+		font-weight: 400;
+	}
+
 	& main {
 		padding: 32px 64px;
 	}
--- apps/dev/pages/utilities/index.vue
+++ apps/dev/pages/utilities/index.vue
@@ -2,12 +2,60 @@
 	<div class="p-utilities">
 		<h1>@kinematic-lab/utilities</h1>
 
-		<h2 id="lab-vector">Lab.Vector</h2>
-		<pre v-text="vector.xy"></pre>
+		<div id="lab-clock">
+			<h2>Lab.Clock</h2>
+			<pre v-text="`Delta: ${delta}`"></pre>
+		</div>
+
+		<div id="lab-cubic-bezier">
+			<h2>Lab.CubicBezier</h2>
+			<div :style="`--percentage: ${bezier(percentage)}`"></div>
+		</div>
+
+		<div id="lab-vector">
+			<h2>Lab.Vector</h2>
+			<pre v-text="`original: \t${vector}`"></pre>
+			<!-- <pre v-text="`vector.xy: \t(${vector.xy})`"></pre>
+			<pre v-text="`vector.zy: \t(${vector.zy})`"></pre>
+			<pre v-text="`vector.rgb: \t(${vector.rgb})`"></pre>
+			<pre v-text="`vector.abg: \t(${vector.abg})`"></pre> -->
+		</div>
 	</div>
 </template>
 
 <script setup>
 import * as Lab from '@kinematic-lab/utilities';
+
+const clock = Lab.Clock();
+const bezier = Lab.CubicBezier(0.7, 0, 0.3, 1);
 const vector = Lab.Vector(1, 2, 3, 4);
+
+const delta = ref(0);
+const percentage = ref(0);
+
+function update() {
+	window.requestAnimationFrame(update);
+	const elapsed = Math.round(clock.getElapsedTime());
+
+	delta.value = clock.getDelta();
+	percentage.value = elapsed * 10 ** -3;
+	percentage.value = Math.abs((percentage.value % 2) - 1);
+}
+
+onMounted(() => update());
 </script>
+
+<style lang="postcss">
+.p-utilities > div:not(:first-child) {
+	margin-top: 48px;
+}
+
+.p-utilities #lab-cubic-bezier > div {
+	width: 48px;
+	height: 48px;
+	border-radius: 4px;
+	background-color: #000;
+	transform: translate3d(calc(var(--percentage) * 256px), 0, 0)
+		rotate(calc(var(--percentage) * 90deg));
+}
+</style>
