3bb778330bdd0b52a1fc13643d8b0e9f9e875073
Aske Lange <44546482+AskeLange@users.noreply.github.com>
2023-08-29 08:10:50 +0200

    chore(`apps/dev`): added various test data

--- apps/dev/pages/utilities/index.vue
+++ apps/dev/pages/utilities/index.vue
@@ -4,21 +4,36 @@
 
 		<div id="lab-clock">
 			<h2>Lab.Clock</h2>
-			<pre v-text="`Delta: ${delta}`"></pre>
+			<div>
+				<pre v-text="`Delta: \t\t${delta}ms`"></pre>
+				<pre v-text="`Elapsed: \t${elapsed}ms`"></pre>
+			</div>
 		</div>
 
 		<div id="lab-cubic-bezier">
 			<h2>Lab.CubicBezier</h2>
-			<div :style="`--percentage: ${bezier(percentage)}`"></div>
+
+			<div>
+				<pre v-text="`Handles: \t(0.75, 0, 0.25, 0)`"></pre>
+				<pre
+					v-text="`Percentage: \t${bezier(percentage) * 100}%`"
+				></pre>
+			</div>
+
+			<div class="boxed">
+				<div :style="`--percentage: ${bezier(percentage)}`"></div>
+			</div>
 		</div>
 
 		<div id="lab-vector">
 			<h2>Lab.Vector</h2>
 			<pre v-text="`original: \t${vector}`"></pre>
-			<!-- <pre v-text="`vector.xy: \t(${vector.xy})`"></pre>
-			<pre v-text="`vector.zy: \t(${vector.zy})`"></pre>
-			<pre v-text="`vector.rgb: \t(${vector.rgb})`"></pre>
-			<pre v-text="`vector.abg: \t(${vector.abg})`"></pre> -->
+			<pre v-text="`vector.xy: \t${vector.xy}`"></pre>
+			<pre v-text="`vector.zy: \t${vector.zy}`"></pre>
+			<pre v-text="`vector.rgb: \t${vector.rgb}`"></pre>
+			<pre v-text="`vector.abg: \t${vector.abg}`"></pre>
+			<pre v-text="`vector[2]: \t${vector[2]}`"></pre>
+			<pre v-text="`vector[3]: \t${vector[3]}`"></pre>
 		</div>
 	</div>
 </template>
@@ -27,35 +42,59 @@
 import * as Lab from '@kinematic-lab/utilities';
 
 const clock = Lab.Clock();
-const bezier = Lab.CubicBezier(0.7, 0, 0.3, 1);
-const vector = Lab.Vector(1, 2, 3, 4);
+const bezier = Lab.CubicBezier(0.75, 0, 0.25, 1);
+const vector = Lab.Vector(0, 1, 2, 3);
 
 const delta = ref(0);
+const elapsed = ref(0);
 const percentage = ref(0);
 
 function update() {
 	window.requestAnimationFrame(update);
-	const elapsed = Math.round(clock.getElapsedTime());
-
 	delta.value = clock.getDelta();
-	percentage.value = elapsed * 10 ** -3;
+	elapsed.value = clock.getElapsedTime();
+
+	percentage.value = Math.round(elapsed.value) * 10 ** -3;
 	percentage.value = Math.abs((percentage.value % 2) - 1);
 }
 
-onMounted(() => update());
+onMounted(() => {
+	console.log(`${vector}`);
+	update();
+});
 </script>
 
 <style lang="postcss">
 .p-utilities > div:not(:first-child) {
 	margin-top: 48px;
+
+	& > div:not(:first-child) {
+		margin-top: 24px;
+	}
 }
 
-.p-utilities #lab-cubic-bezier > div {
-	width: 48px;
-	height: 48px;
-	border-radius: 4px;
-	background-color: #000;
-	transform: translate3d(calc(var(--percentage) * 256px), 0, 0)
-		rotate(calc(var(--percentage) * 90deg));
+.p-utilities pre {
+	line-height: 1.5;
+}
+
+.p-utilities #lab-cubic-bezier > .boxed {
+	margin-top: 24px;
+	padding: 24px 32px;
+	width: calc(320px + 48px);
+	border-radius: 2px;
+	background-color: #eee;
+
+	& > div {
+		width: 48px;
+		height: 48px;
+
+		--pm: max(min(calc(var(--percentage) * 1.2 - 0.1), 1), 0);
+		border-radius: calc(4px + var(--pm) * 22px);
+
+		background-color: #000;
+		transform: translate3d(calc(var(--percentage) * 320px), 0, 0)
+			rotate(calc(var(--percentage) * 90deg))
+			scale(calc(var(--percentage) * 0.1 + 1));
+	}
 }
 </style>
