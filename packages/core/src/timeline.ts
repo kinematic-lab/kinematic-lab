8ae95940cb22be7589f8a2b7a64c5731bab1d2f2
Aske Lange <44546482+AskeLange@users.noreply.github.com>
2024-07-10 12:29:01 +0200

    feat(`packages/core`): implemented `Timeline`

--- packages/core/src/timeline.ts
+++ packages/core/src/timeline.ts
@@ -1,50 +1,41 @@
-import { parseVector } from './utils';
-import type { LabShapingFunction, LabVector } from './types';
+import * as ParserUtils from './utils/parsers';
+import * as ArrayUtils from './utils/array';
+import type { LabTimelineStep } from './types';
 
-export interface TimelineStep {
-	value: number | number[] | LabVector;
-	weight: number;
-	easing?: LabShapingFunction;
-}
-
-function Timeline(
-	from: LabVector | number[] | number,
-	steps: TimelineStep[]
-): (x: number) => LabVector {
-	const weightSum = steps.reduce((acc, { weight }) => acc + weight, 0);
-	const stepsFormatted = steps.map((step) => ({
-		value: parseVector(step.value),
-		easing: step.easing ?? ((x) => x),
-		weight: step.weight,
+function Timeline(steps: LabTimelineStep[]): (x: number) => number[] {
+	const from = ParserUtils.parseArray(steps[0].value);
+	const entries = steps.slice(1).map((entry) => ({
+		value: ParserUtils.parseArray(entry.value),
+		weight: entry.weight ?? 1,
+		easing: entry.easing,
 		to: 0,
 	}));
 
-	stepsFormatted.reduce((acc, step, n) => {
-		return (stepsFormatted[n].to = acc + step.weight / weightSum);
-	}, 0);
+	const ws = entries.reduce((acc, { weight }) => acc + weight, 0);
+	entries.reduce((a, { weight: w }, n) => (entries[n].to = a + w / ws), 0);
 
 	return (x: number) => {
-		if (x < 0) return parseVector(from);
-		if (x >= 1) return stepsFormatted[stepsFormatted.length - 1].value;
+		if (x <= 0) return from;
+		if (x >= 1) return entries[entries.length - 1].value;
 
-		let start;
-		let end;
-
-		for (let n = 0; n < stepsFormatted.length; n++) {
-			if (stepsFormatted[n].to <= x && stepsFormatted[n + 1]?.to > x) {
-				start = stepsFormatted[n];
-				end = stepsFormatted[n + 1];
+		let start, end;
+		for (let n = 0; n < entries.length; n++) {
+			if (entries[n].to <= x && entries[n + 1]?.to > x) {
+				start = entries[n];
+				end = entries[n + 1];
 				break;
 			}
 		}
 
-		start ??= { value: parseVector(from), to: 0 };
-		end ??= stepsFormatted[0];
+		start ??= { value: from, to: 0 };
+		end ??= entries[0];
 
-		const result = end.value.clone();
-		result.subtract(start.value);
-		result.multiply(end.easing((x - start.to) / (end.to - start.to)));
-		result.add(start.value);
+		const percentage = (x - start.to) / (end.to - start.to);
+		const easing = end.easing ?? ((x) => x);
+
+		let result = ArrayUtils.subtract(end.value, start.value);
+		result = ArrayUtils.scale(result, easing(percentage));
+		result = ArrayUtils.add(result, start.value);
 		return result;
 	};
 }
--- packages/core/src/types/timeline.ts
+++ packages/core/src/types/timeline.ts
@@ -1,1 +1,8 @@
-export {};
+import type { LabShapingFunction } from '.';
+import type { LabVector } from './vector';
+
+export interface LabTimelineStep {
+	value: number | number[] | LabVector;
+	weight: number;
+	easing?: LabShapingFunction;
+}
